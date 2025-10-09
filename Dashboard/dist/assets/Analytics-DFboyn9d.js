6a417bd1dc429136b3deabc17b55428d1181134b
Baldev Sharma <baldevsharma00012@gmail.com>
2025-10-09 16:54:15 +0530

    first commit

---
+++ Dashboard/dist/assets/Analytics-DFboyn9d.js
@@ -0,0 +1,1 @@
+import{j as t}from"./index-Bvq_bBfA.js";const r=()=>t.jsx("div",{children:"Analytics"});export{r as default};
---
+++ Dashboard/dist/assets/Setting-QOSIrvZ9.js
@@ -0,0 +1,1 @@
+import{j as t}from"./index-Bvq_bBfA.js";const r=()=>t.jsx("div",{children:"Setting"});export{r as default};
