a5a6d386b065355819041f36f7e68b36ac46c422
Ab <abhishektiwari@gmail.com>
2025-11-05 17:57:51 +0530

    asas

--- Backend/src/models/User.ts
+++ Backend/src/models/User.ts
@@ -13,7 +13,7 @@ const userSchema = new Schema<IUser>({
   name: { type: String, required: true },
   email: { type: String, required: true, unique: true },
   password: { type: String, required: true },
-  role: { type: String, default: "user" }, // 
+  role: { type: String, default: "user" }, 
 });
 
 userSchema.pre("save", async function (next) {
