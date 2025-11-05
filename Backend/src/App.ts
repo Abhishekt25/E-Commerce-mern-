f354687e1d8658a8bd3db5d22c092e223149c901
Abhishek Tiwari <abhishektiwari4843@gmail.com>
2025-11-05 17:52:25 +0530

    abb

--- Backend/src/App.ts
+++ Backend/src/App.ts
@@ -39,7 +39,7 @@ app.use(cors({
 app.use('/api', AuthRoutes);
 app.use('/api/', AdminRoutes);
 
-// Start Server with DB connection     
+// Start Server with DB connection 
 const startServer = async (): Promise<void> => {
   try {
     await connectDB();
