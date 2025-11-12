9cac8e4c027cbb453f70ca63cd50798b08bb979d
Abhishek Tiwari <71788951+Abhishekt25@users.noreply.github.com>
2025-11-13 02:46:55 +0530

    Update Home.tsx

--- Dashboard/src/frontend/Home.tsx
+++ Dashboard/src/frontend/Home.tsx
@@ -8,7 +8,7 @@ const FrontendHome = () => {
       <Banner />
 
       <p className="mt-4 text-center text-gray-600 max-w-2xl mx-auto">
-        Discover our premium skincare and wellness products designed to enhance your natural beauty and boost your confidence.ss
+        Discover our premium skincare and wellness products designed to enhance your natural beauty and boost your confidence.
       </p>
 
       <BestProduct />
