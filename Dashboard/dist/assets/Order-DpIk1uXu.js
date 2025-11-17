7cd723a41d35b81691ca8668d0b3a53db64c042d
Abhishek <abhishektiwari@gmail.com>
2025-11-17 13:21:22 +0530

    Remove dist folder entirely

--- Dashboard/dist/assets/Order-DpIk1uXu.js
+++
@@ -1,1 +0,0 @@
-import{j as r}from"./index-D01GwCp3.js";const t=()=>r.jsx("div",{children:"Order"});export{t as default};
--- Dashboard/dist/assets/Products-DyhbJTX0.js
+++
@@ -1,1 +0,0 @@
-import{j as t}from"./index-D01GwCp3.js";const s=()=>t.jsx("div",{});export{s as default};
